3d39d2014a189e15ee810cca37d0e167da08f427
Deepak Ahirwar <deepak.ahirwar@salt-tech.com>
2022-05-26 16:08:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pratice';
+export class AppComponent implements OnInit {
+  number: any = '';
+
+  Celius: any;
+  Fahernit: any;
+  result1: any;
+  result2: any;
+
+  celius() {
+    this.result1 = (this.Celius * 9.0) / 5.0 + 32;
+  }
+
+  fahernit() {
+    this.result2 = ((this.Fahernit - 32) * 5) / 9;
+  }
+  ngOnInit(): void {}
 }
